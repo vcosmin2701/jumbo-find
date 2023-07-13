88c3e0a1a2a05dda189d4f5f9ea255d56c682974
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-07-13 16:33:58 +0300

    Connection to the API

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("is it working ? ^_^")
+import configparser
+from googleapiclient.discovery import build
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['api-key']['api_key']
+
+service = build('youtube', 'v3', developerKey=api_key)
+
+request = service.channels().list(
+    part='statistics',
+    forUsername='sentdex'
+)
+
+response = request.execute()
+
+print(response)
